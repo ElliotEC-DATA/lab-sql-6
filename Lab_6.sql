fe13f2341dacbc12ce04ca15faa45d528cec00fe
ElliotEC-DATA <113889870+ElliotEC-DATA@users.noreply.github.com>
2022-11-20 16:46:47 +0100

    Add files via upload

---
+++ Lab_6.sql
@@ -0,0 +1,34 @@
+use sakila;
+
+drop table if exists films_2020;
+CREATE TABLE `films_2020` (
+  `film_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `description` text,
+  `release_year` year(4) DEFAULT NULL,
+  `language_id` tinyint(3) unsigned NOT NULL,
+  `original_language_id` tinyint(3) unsigned DEFAULT NULL,
+  `rental_duration` int(6),
+  `rental_rate` decimal(4,2),
+  `length` smallint(5) unsigned DEFAULT NULL,
+  `replacement_cost` decimal(5,2) DEFAULT NULL,
+  `rating` enum('G','PG','PG-13','R','NC-17') DEFAULT NULL,
+  PRIMARY KEY (`film_id`),
+  CONSTRAINT FOREIGN KEY (`original_language_id`) REFERENCES `language` (`language_id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1003 DEFAULT CHARSET=utf8;
+
+select * from films_2020
+limit 1001; 
+
+insert into films_2020 (rental_duration, rental_rate, replacement_cost)
+values (3, 2.99, 8.99);
+
+-- "Error Code: 1364. Field 'title' doesn't have a default value" but title canno't have a default value here
+
+
+
+
+
+
+
+
